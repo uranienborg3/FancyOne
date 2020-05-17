87524585cb61c3ce868cf95914ab914c758d3cf1
uranienborg3 <uranienborg3@gmail.com>
2020-05-17 17:34:59 +0300

    new product page test: open big pic

--- src/main/java/pages/ProductPage.java
+++ src/main/java/pages/ProductPage.java
@@ -1,11 +1,18 @@
 package pages;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.*;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.concurrent.TimeUnit;
 
 public class ProductPage extends TopBase {
 
     private By bigpic = By.id("bigpic");
+    private By bigPicOpened = By.cssSelector("div.fancybox-opened");
+    private By nextBigpic = By.cssSelector("div.fancybox-opened a.fancybox-next");
+    private By closeBigPic = By.cssSelector("a.fancybox-close");
 
     ProductPage(WebDriver driver) {
         super(driver);
@@ -13,5 +20,25 @@ public class ProductPage extends TopBase {
 
     public boolean bigPicIsDisplayed() {
         return driver.findElement(bigpic).isDisplayed();
+    }
+
+    public void clickBigPic() {
+        driver.findElement(bigpic).click();
+    }
+
+    public void closeBigPic() {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        wait.until(ExpectedConditions.visibilityOfElementLocated(closeBigPic));
+        driver.findElement(closeBigPic).click();
+    }
+
+    public boolean bigPicIsClosed() {
+        WebDriverWait wait = new WebDriverWait(driver, 3);
+        try {
+            wait.until(ExpectedConditions.visibilityOfElementLocated(bigPicOpened));
+        } catch (TimeoutException e) {
+            return true;
+        }
+        return false;
     }
 }
---
+++ src/test/java/product/ProductPageTests.java
@@ -0,0 +1,17 @@
+package product;
+
+import base.BaseTests;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertTrue;
+
+public class ProductPageTests extends BaseTests {
+    @Test
+    public void testViewPictureInLarge() {
+        var product = topbase.switchToHomePage().viewProductDetails(3);
+        assertTrue(product.bigPicIsDisplayed(), "Big pic is not there: probably not a product detail page");
+        product.clickBigPic();
+        product.closeBigPic();
+        assertTrue(product.bigPicIsClosed(), "Big pic is not closed");
+    }
+}
