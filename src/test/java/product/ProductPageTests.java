0fd7001c23b2167188919664d88ec99bcf6eb8d7
uranienborg3 <uranienborg3@gmail.com>
2020-05-17 17:54:03 +0300

    2 product page tests made into 1

--- src/test/java/product/ProductPageTests.java
+++ src/test/java/product/ProductPageTests.java
@@ -11,17 +11,8 @@ public class ProductPageTests extends BaseTests {
         var product = topbase.switchToHomePage().viewProductDetails(3);
         assertTrue(product.bigPicIsDisplayed(), "Big pic is not there: probably not a product detail page");
         product.clickBigPic();
-        product.closeBigPic();
-        assertTrue(product.bigPicIsClosed(), "Big pic is not closed");
-    }
-
-    @Test
-    public void testClickNextPic() {
-        var product = topbase.switchToHomePage().viewProductDetails(3);
-        assertTrue(product.bigPicIsDisplayed(), "Big pic is not there: probably not a product detail page");
-        product.clickBigPic();
         product.clickNextBigPic(4);
         product.closeBigPic();
-        product.bigPicIsClosed();
+        assertTrue(product.bigPicIsClosed(), "Big pic is not closed");
     }
 }
